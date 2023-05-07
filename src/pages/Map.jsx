ab35e321fdba44e622d0c9d7e8a861828c2d0ad4
snixie <fsd.rabgay@gmail.com>
2023-05-07 08:05:00 +0600

    package

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,23 +1,5 @@
 const Map = () => {
-  const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBGunsUmQ7N12nT29zMLRFg_srdOdtHSUo`;
-  const loadingElement = <div className="h-full" />;
-  const containerElement = <div className="h-full" />;
-  const mapElement = <div className="h-full" />;
-  const lat = 37.7749;
-  const lng = -122.4194;
-
-  return (
-    <div>
-      <Map
-        googleMapURL={googleMapURL}
-        loadingElement={loadingElement}
-        containerElement={containerElement}
-        mapElement={mapElement}
-        lat={lat}
-        lng={lng}
-      />
-    </div>
-  );
+  return <div>Map</div>;
 };
 
 export default Map;
